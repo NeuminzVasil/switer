46fde35cd8b3b6f11b2dc32bdb7a791d05cd7f7d
Zexa91x0 <Zexa91x0@mail.ru>
2021-02-14 14:30:19 +0500

    temp

--- src/main/java/com/example/servingwebcontent/controller/RegistrationController.java
+++ src/main/java/com/example/servingwebcontent/controller/RegistrationController.java
@@ -5,10 +5,12 @@ import com.example.servingwebcontent.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
 import java.util.Map;
@@ -25,15 +27,25 @@ public class RegistrationController {
 
 
     @PostMapping("/registration")
-    public String addUser(@Valid Customer customer, BindingResult bindingResult, Model model) {
+    public String addUser(@RequestParam("password2") String passwordConfirm,
+                          @Valid Customer customer,
+                          BindingResult bindingResult,
+                          Model model) {
+
 
         System.out.println(customer);
 
-        if (customer.getPassword() != null && customer.getPassword().equals(customer.getPassword2())) {
+        boolean isConfirmEmpty = StringUtils.isEmpty(passwordConfirm);
+
+        if (isConfirmEmpty) {
+            model.addAttribute("password2Error", "password confirmation can`t be empty");
+        }
+
+        if (customer.getPassword() != null && customer.getPassword().equals(passwordConfirm)) {
             model.addAttribute("passwordError", "passwords are different!");
         }
 
-        if (bindingResult.hasErrors()){
+        if (isConfirmEmpty || bindingResult.hasErrors()) {
             Map<String, String> errors = ControllerUtils.getErrors(bindingResult);
             model.addAllAttributes(errors);
             return "registration";
@@ -52,8 +64,10 @@ public class RegistrationController {
         boolean isActivated = customerService.activateUser(code);
 
         if (isActivated) {
+            model.addAttribute("messageType", "success");
             model.addAttribute("message", "Activation Successfully");
         } else
+            model.addAttribute("messageType", "danger");
             model.addAttribute("message", "Activation code not found");
 
         return "login";
